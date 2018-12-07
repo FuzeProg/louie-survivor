cc3fa4e8156ca9e811b6a31e33f23b8b90883c53
Nicolas SAGOT <nicolas.sagot@alterway.fr>
2018-12-07 02:42:36 +0100

    :truck: Pm2 ready

--- src/mqtt.js
+++ src/mqtt.js
@@ -3,11 +3,6 @@ const mqtt = require('mqtt');
 const HOST = 'localhost';
 const client = mqtt.connect('mqtt://' + HOST, {port: 1883});
 
-const low = require('lowdb'),
-	FileSync = require('lowdb/adapters/FileSync'),
-	adapter = new FileSync('db.json'),
-	db = low(adapter);
-
 client.on('connect', function () {
 	console.log("Connected to " + HOST);
 	client.subscribe('#');
